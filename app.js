18028ffa1e77af37636d2a6c21b457565c6fba45
noobiehacker <davidkwokhochan@gmail.com>
2015-11-24 22:59:12 -0800

    Added node + express BE

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app = express()
+
+app.set('view engine', 'ejs');
+app.use(express.static( "public" ));
+
+app.get('/', function(req,res){
+	res.render('default', {
+		title: 'Home',
+		classname: 'home',
+		users: ['Ray', 'Morten', 'Jame']
+	});
+});
+
+app.get('/about', function(req,res){
+	res.render('default', {
+		title: 'About Us',
+		classname: 'about'
+	});
+});
+
+var server = app.listen(3000, function() {
+	console.log('Listening on port 3000');
+});
